677d1e9e5cc3df035e67115f7136e3d18a8d24e2
shian simms <ssimmswork@gmail.com>
2019-04-04 23:24:09 -0400

    First Commit

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,119 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+
+//=== GameObject ===
+function GameObject(gameAttr) {
+    this.createdAt = gameAttr.createdAt;
+    this.name = gameAttr.name;
+    this.dimensions = gameAttr.dimensions; // (These represent the character's size in the video game)
+}
+GameObject.prototype.destroy = function() {
+        return `${this.name} was removed from the game.`;
+    } // prototype method that returns: `${this.name} was removed from the game.`
+
+
+
+//=== CharacterStats ===
+function CharacterStats(stats) {
+    GameObject.call(this, stats);
+    this.healthPoints = stats.healthPoints;
+    this.points = stats.points;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+        return `${this.name} took damage.`;
+    } // prototype method -> returns the string '<object name> took damage.'
+
+// === Humanoid (Having an appearance or character resembling that of a human.) ===
+function Humanoid(humanoid) {
+    CharacterStats.call(this, humanoid)
+    this.team = humanoid.team;
+    this.weapons = humanoid.weapons;
+    this.language = humanoid.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    } // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+
+
+
+
+/*
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+        'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+        'Giant Sword',
+        'Shield',
+    ],
+    language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+        'Bow',
+        'Dagger',
+    ],
+    language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
